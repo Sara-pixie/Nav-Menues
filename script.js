92ea3020d0176e37c1a4abce0f2e3539ca7a6674
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2021-12-18 00:02:52 +0100

    Second navbar example

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-const listElement = document.querySelectorAll(".listElement");
-function activeLink(){
-    listElement.forEach((element)=>{
+const listElement_1 = document.querySelectorAll(".listElement_1");
+function activeLink1(){
+    listElement_1.forEach((element)=>{
         element.classList.remove("active");
     });
     this.classList.add("active");
 }
-listElement.forEach((element)=>{
-    element.addEventListener("click", activeLink);
+listElement_1.forEach((element)=>{
+    element.addEventListener("click", activeLink1);
+});
+const listElement_2 = document.querySelectorAll(".listElement_2");
+function activeLink2(){
+    listElement_2.forEach((element)=>{
+        element.classList.remove("active");
+    });
+    this.classList.add("active");
+}
+listElement_2.forEach((element)=>{
+    element.addEventListener("click", activeLink2);
 });
